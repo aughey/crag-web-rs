e31fc39bf3a0a4a2a2374d9e4ca0e35c357777d2
John Aughey <jha@aughey.com>
2024-05-14 21:15:56 +0000

    Changed use statements

--- crag-web/src/server.rs
+++ crag-web/src/server.rs
@@ -1,17 +1,16 @@
+use crate::handler;
+use crate::request;
+use crate::threadpool;
 use std::collections::HashMap;
 use std::error;
 use std::fmt;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 
-use crate::handler;
-use crate::request::{self, Request};
-use crate::threadpool::{self, PoolCreationError};
-
 #[derive(Debug)]
 pub enum ServerError {
     ServerCreation(std::io::Error),
-    PoolSizeError(PoolCreationError),
+    PoolSizeError(threadpool::PoolCreationError),
 }
 
 impl fmt::Display for ServerError {
@@ -100,7 +99,7 @@ fn handle_connection(handlers: HashMap<request::Request, handler::Handler>, mut 
 }
 
 // TODO: Fix return type
-fn parse_request(stream: &mut TcpStream) -> Result<Request, std::io::Error> {
+fn parse_request(stream: &mut TcpStream) -> Result<request::Request, std::io::Error> {
     // create buffer
     let mut request: Vec<String> = vec![];
     let mut buffer = BufReader::new(stream);
